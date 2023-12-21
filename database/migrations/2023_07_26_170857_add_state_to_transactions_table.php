3a64e69e9d30ec4aa0aef904c32a644a8b905f13
Daniel Chinchilla <josedanielchinchilla@gmail.com>
2023-12-20 18:31:57 -0600

    fix migratiion

--- database/migrations/2023_07_26_170857_add_state_to_transactions_table.php
+++ database/migrations/2023_07_26_170857_add_state_to_transactions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->unsignedBigInteger('country_id'); 
             $table->foreign('country_id')->references('id')->on('countries');
             $table->enum('type', ['ingreso', 'egreso', 'anulado'])->default('ingreso');
-            $table->enum('method', ['efectivo', 'cheque', 'anulado'])->default('ingreso');
+            $table->enum('method', ['efectivo', 'cheque', 'anulado'])->default('efectivo');
             $table->text('check_number')->nullable();
             $table->text('bank')->nullable();
             $table->date('date')->useCurrent();
